d98cbc42a56564e0588b672e3c29e337d66f1b55
Conor Todd <chtodd@inflowfed.com>
2024-04-24 14:15:39 -0400

    updated tests

--- pages/tests.py
+++ pages/tests.py
@@ -8,9 +8,17 @@ class HomepageTests(SimpleTestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
-    def test_url_available_by_name(self):  # new
+    def test_url_available_by_name(self):  
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):  # new
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_content(self):  # new
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Homepage</h1>")
 
 
 class AboutpageTests(SimpleTestCase):
@@ -18,6 +26,14 @@ class AboutpageTests(SimpleTestCase):
         response = self.client.get("/about/")
         self.assertEqual(response.status_code, 200)
 
-    def test_url_available_by_name(self):  # new
+    def test_url_available_by_name(self):  
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):  # new
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_template_content(self):  # new
+        response = self.client.get(reverse("about"))
+        self.assertContains(response, "<h1>About page</h1>")
